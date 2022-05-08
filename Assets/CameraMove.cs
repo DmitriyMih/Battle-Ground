9a68d8aa5cc8f1ec2c337bbe83de2227ed336d95
Jew <dima.com655@gmail.com>
2022-05-08 13:37:52 +0300

    Add Camera Move

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public Transform cameraTransform;
     public int pointCount;
 
+    public float speed = 2.5f;
+    public float currentDistance;
+    public Vector2Int clampPositions;
+
+    public void Awake()
+    {
+        currentDistance = cameraTransform.position.z;    
+    }
+
+    public void Update()
+    {
+        float horizontal = Input.GetAxis("Vertical");
+
+        currentDistance = Mathf.Clamp(currentDistance + horizontal * 2.5f, clampPositions.x, clampPositions.y);
+        cameraTransform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, currentDistance);
+    }
 }
